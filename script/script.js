c95bd5f910507c1388c206c9b446cb5fa9c9af95
naiden <na4ko359@gmail.com>
2022-03-05 19:40:16 +0200

    mobile design

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const adviceButton = document.querySelector("#advice_button");
 const adviceText = document.querySelector("#advice_text");
 const adviceId = document.querySelector("#advice_id");
+const dice = document.querySelector("#dice");
 
 window.onload = function() {
     fetchAdvice();
@@ -9,9 +10,12 @@ window.onload = function() {
 async function fetchAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"});
     const data = await response.json();
-    adviceId.textContent = `ADVICE # ${data["slip"]["id"]}`;
+    adviceId.textContent = `ADVICE #${data["slip"]["id"]}`;
     adviceText.textContent = `"${data["slip"]["advice"]}"`;
     return data;
 }
 
 adviceButton.addEventListener("click", fetchAdvice);
+adviceButton.addEventListener("click", () => {
+    dice.classList.toggle("rotate");
+});
