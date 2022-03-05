344d69c7024df764f664a0e9e35a1ca093a70e4f
naiden <na4ko359@gmail.com>
2022-03-05 18:35:06 +0200

    fetch on window load

--- script/script.js
+++ script/script.js
@@ -2,6 +2,10 @@ const adviceButton = document.querySelector("#advice_button");
 const adviceText = document.querySelector("#advice_text");
 const adviceId = document.querySelector("#advice_id");
 
+window.onload = function() {
+    fetchAdvice();
+}
+
 async function fetchAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"});
     const data = await response.json();
