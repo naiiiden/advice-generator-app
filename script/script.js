7f00460979ffe14729369666ef29543055b15190
naiden <na4ko359@gmail.com>
2022-03-05 18:41:26 +0200

    update adviceId, adviceText textContent

--- script/script.js
+++ script/script.js
@@ -9,8 +9,8 @@ window.onload = function() {
 async function fetchAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"});
     const data = await response.json();
-    adviceId.textContent = data["slip"]["id"];
-    adviceText.textContent = data["slip"]["advice"];
+    adviceId.textContent = `ADVICE # ${data["slip"]["id"]}`;
+    adviceText.textContent = `"${data["slip"]["advice"]}"`;
     return data;
 }
 
