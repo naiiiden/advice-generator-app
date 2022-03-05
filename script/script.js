2cf2f4a9c9f56a9da3e3f5649c7a55dd028939ea
naiden <na4ko359@gmail.com>
2022-03-05 18:32:22 +0200

    fetch advice text/advice id

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const adviceButton = document.querySelector("#advice_button");
+const adviceText = document.querySelector("#advice_text");
+const adviceId = document.querySelector("#advice_id");
+
+async function fetchAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"});
+    const data = await response.json();
+    adviceId.textContent = data["slip"]["id"];
+    adviceText.textContent = data["slip"]["advice"];
+    return data;
+}
+
+adviceButton.addEventListener("click", fetchAdvice);
